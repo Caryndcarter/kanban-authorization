23affac9ccfe70cffa016bfeae2da6bf3475bf26
Caryn Carter <carter.caryn@gmail.com>
2024-11-30 21:30:42 -0600

    modifying Sequelize DB connection

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,15 +6,7 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Hosted in Render, and this might be necessary for many cloud-hosted databases
-      },
-    },
-  })
+  ? new Sequelize(process.env.DATABASE_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
